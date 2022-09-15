cb3fae2cc21a6d94a0151f23c8bcc2ed4f93c705
hongyongxun2020144120 <113495854+hongyongxun2020144120@users.noreply.github.com>
2022-09-15 17:18:16 +0800

    学号：2020144120 姓名：洪永勋

---
+++ 2020144120.py
@@ -0,0 +1,19 @@
+imax = 0
+imin = 0
+nums = []
+
+while True:
+    num = input("Enter a number:")
+    if num == 'done':
+        break
+    else:
+        try:
+            num = float(num)
+            nums.append(num)
+        except:
+            print("Invalid input!")
+
+for i in nums:
+    imax = int(max(nums))
+    imin = int(min(nums))
+print(imax,   imin)
---
+++ 2020144120h.py
@@ -0,0 +1,4 @@
+str='X-DSPAM-Confidence：0.8475'
+num_str_1 = str[19:]
+float(num_str_1 )
+print(num_str_1)
